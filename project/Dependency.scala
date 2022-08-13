191a0c62aca84961753e908db5cdb82ee51040d4
Scala Steward <rafael.fiume@yahoo.it>
2022-08-13 16:38:35 +0100

    Update cats-free to 2.8.0

--- project/Dependency.scala
+++ project/Dependency.scala
@@ -3,7 +3,7 @@ import sbt._
 // format: off
 object Dependency {
   private object Version {
-    val cats                  = "2.6.1"
+    val cats                  = "2.8.0"
     val catsEffect            = "3.3.14"
     val circe                 = "0.14.1"
     val ciris                 = "2.0.1"
