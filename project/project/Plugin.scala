333a4fcb822aead80cc91e6c56d78c6126be7dd8
Scala Steward <rafael.fiume@yahoo.it>
2022-08-13 16:38:23 +0100

    Revert commit(s) bef2c05

--- project/project/Plugin.scala
+++ project/project/Plugin.scala
@@ -6,7 +6,7 @@ object Plugin {
     val Assembly        = "0.14.6"
     val DependencyGraph = "0.9.0"
     val DotEnv          = "2.1.204"
-    val Git             = "2.0.0"
+    val Git             = "1.0.2"
     val Scalafix        = "0.9.34"
     val ScalaFmt        = "2.4.6"
     val Tpolecat        = "0.1.22"
@@ -15,7 +15,7 @@ object Plugin {
   val Assembly        = "com.eed3si9n"              %  "sbt-assembly"         % Version.Assembly
   val DependencyGraph = "net.virtual-void"          %  "sbt-dependency-graph" % Version.DependencyGraph
   val DotEnv          = "au.com.onegeek"            %% "sbt-dotenv"           % Version.DotEnv
-  val Git             = "com.github.sbt"          %  "sbt-git"              % Version.Git
+  val Git             = "com.typesafe.sbt"          %  "sbt-git"              % Version.Git
   val Scalafix        = "ch.epfl.scala"             %  "sbt-scalafix"         % Version.Scalafix
   val ScalaFmt        = "org.scalameta"             %  "sbt-scalafmt"         % Version.ScalaFmt
   val Tpolecat        = "io.github.davidgregory084" %  "sbt-tpolecat"         % Version.Tpolecat
