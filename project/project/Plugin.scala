6a3c17851e8f3ae51cbbe0eb3e5b115f4631ba92
Scala Steward <rafael.fiume@yahoo.it>
2022-08-13 17:04:17 +0100

    Update sbt-git to 2.0.0

--- project/project/Plugin.scala
+++ project/project/Plugin.scala
@@ -6,7 +6,7 @@ object Plugin {
     val Assembly        = "0.14.10"
     val DependencyGraph = "0.9.2"
     val DotEnv          = "2.1.233"
-    val Git             = "1.0.2"
+    val Git             = "2.0.0"
     val Scalafix        = "0.10.1"
     val ScalaFmt        = "2.4.6"
     val Tpolecat        = "0.4.1"
@@ -15,7 +15,7 @@ object Plugin {
   val Assembly        = "com.eed3si9n"              %  "sbt-assembly"         % Version.Assembly
   val DependencyGraph = "net.virtual-void"          %  "sbt-dependency-graph" % Version.DependencyGraph
   val DotEnv          = "au.com.onegeek"            %% "sbt-dotenv"           % Version.DotEnv
-  val Git             = "com.typesafe.sbt"          %  "sbt-git"              % Version.Git
+  val Git             = "com.github.sbt"          %  "sbt-git"              % Version.Git
   val Scalafix        = "ch.epfl.scala"             %  "sbt-scalafix"         % Version.Scalafix
   val ScalaFmt        = "org.scalameta"             %  "sbt-scalafmt"         % Version.ScalaFmt
   val Tpolecat        = "io.github.davidgregory084" %  "sbt-tpolecat"         % Version.Tpolecat
