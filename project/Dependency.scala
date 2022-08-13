d9610add4b44b17d6e8d5e81cba9aaf201be412e
Scala Steward <rafael.fiume@yahoo.it>
2022-08-13 13:41:23 +0100

    Update http4s-blaze-client, ... to 1.0.0-M35

--- project/Dependency.scala
+++ project/Dependency.scala
@@ -9,7 +9,7 @@ object Dependency {
     val ciris                 = "2.0.1"
     val doobie                = "1.0.0-RC1"
     val flyway                = "7.5.2"
-    val http4s                = "1.0.0-M30"
+    val http4s                = "1.0.0-M35"
     val log4cats              = "2.1.1"
     val slf4j                 = "1.7.36"
 
