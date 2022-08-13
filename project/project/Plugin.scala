f9a756a9c742104d599705b7926cabc3fa1c24e5
Scala Steward <rafael.fiume@yahoo.it>
2022-08-13 13:40:31 +0100

    Update sbt-assembly to 0.14.10

--- project/project/Plugin.scala
+++ project/project/Plugin.scala
@@ -3,7 +3,7 @@ import sbt._
 // format: off
 object Plugin {
   private object Version {
-    val Assembly        = "0.14.6"
+    val Assembly        = "0.14.10"
     val DependencyGraph = "0.9.0"
     val DotEnv          = "2.1.204"
     val Git             = "1.0.2"
