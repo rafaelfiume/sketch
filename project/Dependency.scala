b1a9b8ef081b0564f343c9c765c1c66239a69f43
Scala Steward <rafael.fiume@yahoo.it>
2022-08-13 13:42:01 +0100

    Update doobie-core, doobie-hikari, ... to 1.0.0-RC2

--- project/Dependency.scala
+++ project/Dependency.scala
@@ -7,7 +7,7 @@ object Dependency {
     val catsEffect            = "3.1.1"
     val circe                 = "0.14.1"
     val ciris                 = "2.0.1"
-    val doobie                = "1.0.0-RC1"
+    val doobie                = "1.0.0-RC2"
     val flyway                = "7.5.2"
     val http4s                = "1.0.0-M30"
     val log4cats              = "2.1.1"
