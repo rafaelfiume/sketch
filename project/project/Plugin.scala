e5622cf6a1e3c96dec000e806fa96a1d067a0e1c
Scala Steward <rafael.fiume@yahoo.it>
2022-09-02 16:32:07 +0100

    Update sbt-native-packager to 1.9.11

--- project/project/Plugin.scala
+++ project/project/Plugin.scala
@@ -6,7 +6,7 @@ object Plugin {
     val DependencyGraph    = "0.9.2"
     val DotEnv             = "3.0.0"
     val Git                = "2.0.0"
-    val SbtNativePackager  = "1.8.1"
+    val SbtNativePackager  = "1.9.11"
     val Scalafix           = "0.10.1"
     val ScalaFmt           = "2.4.6"
     val Tpolecat           = "0.4.1"
@@ -16,7 +16,7 @@ object Plugin {
   val DependencyGraph   = "net.virtual-void"           %  "sbt-dependency-graph"  % Version.DependencyGraph
   val DotEnv            = "nl.gn0s1s"                  %% "sbt-dotenv"            % Version.DotEnv
   val Git               = "com.github.sbt"             %  "sbt-git"               % Version.Git
-  val SbtNativePackager = "com.typesafe.sbt"           %% "sbt-native-packager"   % Version.SbtNativePackager
+  val SbtNativePackager = "com.github.sbt"           %% "sbt-native-packager"   % Version.SbtNativePackager
   val Scalafix          = "ch.epfl.scala"              %  "sbt-scalafix"          % Version.Scalafix
   val ScalaFmt          = "org.scalameta"              %  "sbt-scalafmt"          % Version.ScalaFmt
   val Tpolecat          = "io.github.davidgregory084"  %  "sbt-tpolecat"          % Version.Tpolecat
