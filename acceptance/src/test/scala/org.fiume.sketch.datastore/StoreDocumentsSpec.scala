1bf5b9c27342598911e642d862d4fbde2c445b54
Rafael Fiume <rafael.fiume@yahoo.it>
2022-09-25 19:14:04 +0100

    Add first acceptance test

--- acceptance/src/test/scala/org.fiume.sketch.datastore/StoreDocumentsSpec.scala
+++ acceptance/src/test/scala/org.fiume.sketch.datastore/StoreDocumentsSpec.scala
@@ -1,9 +1,22 @@
 package org.fiume.sketch.datastore
 
-import munit.CatsEffectSuite
+import cats.effect.IO
 import munit.Assertions.*
+import munit.CatsEffectSuite
+import org.http4s.client.*
+import org.http4s.ember.client.*
+import org.http4s.implicits.*
 
 class StoreDocumentsSpec extends CatsEffectSuite:
+
+  // should be moved to a separated suite in the future
+  test("ping") {
+    EmberClientBuilder.default[IO].build.use { client =>
+      client.expect[String](uri"http://localhost:8080/ping").map { res =>
+        assertEquals(res, "\"pong\"") // json string
+      }
+    }
+  }
 
   test("store documents".ignore) {
     fail("coming soon")
