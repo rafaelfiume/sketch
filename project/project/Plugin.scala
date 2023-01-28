dce3394215a0b94c1a64b1c7ae73c9680b5b1370
Rafael Fiume <rafaelfiume@users.noreply.github.com>
2023-01-28 13:49:03 +0000

    Update sbt-native-packager to 1.9.13 (#59)

--- project/project/Plugin.scala
+++ project/project/Plugin.scala
@@ -6,7 +6,7 @@ object Plugin {
     val DependencyGraph    = "0.9.2"
     val Git                = "2.0.0"
     val OrganizeImports    = "0.6.0"
-    val SbtNativePackager  = "1.9.11"
+    val SbtNativePackager  = "1.9.13"
     val Scalafix           = "0.10.4"
     val ScalaFmt           = "2.5.0"
     val Tpolecat           = "0.4.1"
