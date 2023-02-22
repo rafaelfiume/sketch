1198f68ec7ce06fcb2f843df2455a92c2c913f47
Rafael Fiume <rafael.fiume@yahoo.it>
2023-02-22 19:48:56 +0000

    Document pipeline, format code and add a TODO

--- service/src/test/scala/org/fiume/sketch/support/gens/Gens.scala
+++ service/src/test/scala/org/fiume/sketch/support/gens/Gens.scala
@@ -28,7 +28,7 @@ object Gens:
     for
       year <- Gen.choose(2022, 2026)
       month <- Gen.choose(1, 12)
-      day <- if (month == 2) Gen.choose(1, 28) else Gen.choose(1, 30) // too simplistic: improve it
+      day <- if month == 2 then Gen.choose(1, 28) else Gen.choose(1, 30) // too simplistic: improve it
       hour <- Gen.choose(0, 23)
       minute <- Gen.choose(0, 59)
       second <- Gen.choose(0, 59)
