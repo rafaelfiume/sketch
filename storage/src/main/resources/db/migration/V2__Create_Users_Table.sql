9fa89f476aad565762b826fce09292411d792a9e
Rafael Fiume <rafael.fiume@yahoo.it>
2023-07-07 23:01:30 +0100

    Fix documentation

--- storage/src/main/resources/db/migration/V2__Create_Users_Table.sql
+++ storage/src/main/resources/db/migration/V2__Create_Users_Table.sql
@@ -6,9 +6,9 @@ CREATE TABLE users (
   id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   -- same size as email
   username VARCHAR(60) NOT NULL UNIQUE,
-  -- 32 bytes Base64 encoded (padding '=' plust each char representing 6 bits) password = 1 + (32 * 8) / 6 = 44 + extra space = 50
+  -- 32 bytes Base64 encoded (padding '=' plus each char representing 6 bits) password = 1 + (32 * 8) / 6 = 44 + extra space = 50
   password_hash VARCHAR(50) NOT NULL,
-  -- 32 bytes Base64 encoded (each char representing 6 bits) salt = 32 * 8 / 6 = 43 + extra space = 50
+  -- 32 bytes Base64 encoded (padding '=' plus each char representing 6 bits) salt = 1 + (32 * 8) / 6 = 44 + extra space = 50
   salt VARCHAR(50) NOT NULL,
   first_name VARCHAR(45) NOT NULL,
   last_name VARCHAR(45) NOT NULL,
