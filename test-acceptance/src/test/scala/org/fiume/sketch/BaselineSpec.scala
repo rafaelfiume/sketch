bab53fe063ad2dd91cc94ba789988cd33e6db858
Rafael Fiume <rafael.fiume@yahoo.it>
2023-07-07 19:38:14 +0100

    Try to fix flickering acc. baseline test

--- test-acceptance/src/test/scala/org/fiume/sketch/BaselineSpec.scala
+++ test-acceptance/src/test/scala/org/fiume/sketch/BaselineSpec.scala
@@ -1,13 +1,16 @@
 package org.fiume.sketch
 
+import cats.effect.IO
 import munit.Assertions.*
 import munit.CatsEffectSuite
 import org.fiume.sketch.support.Http4sContext
 
+import scala.concurrent.duration.*
+
 class BaselineSpec extends CatsEffectSuite with Http4sContext:
 
   test("ping returns pong") {
-    http { client =>
+    IO.sleep(300.millisecond) *> http { client =>
       client.expect[String]("http://localhost:8080/ping".get).map { res =>
         assertEquals(res, "\"pong\"") // json string
       }
