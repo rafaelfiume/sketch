2ac22eeeae90ee5add0b619f1a182c69c02e772e
Rafael Fiume <rafael.fiume@yahoo.it>
2023-07-14 19:12:24 +0100

    Remove redundant SQL instructions

--- storage/src/main/resources/db/migration/V2__Create_Users_Table.sql
+++ storage/src/main/resources/db/migration/V2__Create_Users_Table.sql
@@ -1,7 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-SET timezone = 'UTC';
-
 CREATE TABLE users (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   -- same size as email
@@ -25,15 +21,6 @@ CREATE TABLE users (
 
 CREATE INDEX idx_users_username ON users (username);
 CREATE INDEX idx_users_email ON users (email);
-
--- Trigger to update `updated_at` timestamp on row update
-CREATE OR REPLACE FUNCTION update_updated_at()
-  RETURNS TRIGGER AS $$
-BEGIN
-  NEW.updated_at := NOW();
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
 
 -- Trigger to update `updated_at` timestamp on row update
 CREATE TRIGGER set_updated_at
