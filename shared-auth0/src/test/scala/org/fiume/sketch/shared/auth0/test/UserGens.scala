05f3cfec7517ea3bf845181b250bb0a4518aa103
Rafael Fiume <rafael.fiume@yahoo.it>
2023-07-26 19:02:57 +0100

    Organise imports

--- shared-auth0/src/test/scala/org/fiume/sketch/shared/auth0/test/UserGens.scala
+++ shared-auth0/src/test/scala/org/fiume/sketch/shared/auth0/test/UserGens.scala
@@ -5,8 +5,8 @@ import org.fiume.sketch.shared.auth0.User
 import org.fiume.sketch.shared.auth0.User.{UserCredentials, UserCredentialsWithId, Username}
 import org.fiume.sketch.shared.auth0.test.PasswordsGens.*
 import org.fiume.sketch.shared.auth0.test.PasswordsGens.HashedPasswords.*
-import org.fiume.sketch.shared.auth0.test.PasswordsGens.Salts.*
 import org.fiume.sketch.shared.auth0.test.PasswordsGens.PlainPasswords.*
+import org.fiume.sketch.shared.auth0.test.PasswordsGens.Salts.*
 import org.fiume.sketch.shared.auth0.test.UserGens.Usernames.*
 import org.scalacheck.{Arbitrary, Gen}
 
