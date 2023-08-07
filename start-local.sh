5b4fdd3029ec24cd147bf1408fb6140b91264edd
Rafael Fiume <rafael.fiume@yahoo.it>
2023-08-07 11:35:26 +0100

    Set up pipeline to store stack and docker-composer logs as artefacts

--- start-local.sh
+++ start-local.sh
@@ -6,4 +6,9 @@ source "keys-setup.sh"
 docker-compose \
   -f local-docker-compose.yml \
   --env-file service/environments/dev.env \
-  up -d
+  up -d 2>&1 | tee tools/scripts/logs/docker-compose.log
+
+sleep 2
+
+docker-compose -f local-docker-compose.yml logs sketch > tools/scripts/logs/sketch.log
+docker-compose -f local-docker-compose.yml logs db > tools/scripts/logs/database.log
