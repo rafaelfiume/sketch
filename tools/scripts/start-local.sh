6537a6ec007e631883979200afe6d402aba5fa92
Rafael Fiume <rafael.fiume@yahoo.it>
2023-08-08 15:08:55 +0100

    Comment pipeline limitation

--- tools/scripts/start-local.sh
+++ tools/scripts/start-local.sh
@@ -12,12 +12,16 @@ LOG_DIR="$SCRIPT_DIR/logs"
 # Execute the environment setup script
 source "$SCRIPT_DIR/keys-setup.sh"
 
+# TODO There is a limitation on the number of concurrents build in the pipeline,
+# given that we cannot control the tag of the image (hardcoded to 'latest').
+# Fix this by parametising the tag of the image. See `local-docker-compose.yml`
+
 docker-compose \
   -f $SCRIPT_DIR/local-docker-compose.yml \
   --env-file $ENV_DIR/dev.env \
   up -d 2>&1 | tee $LOG_DIR/docker-compose.log
 
-sleep 1
+sleep 2
 
 docker-compose -f $SCRIPT_DIR/local-docker-compose.yml logs sketch > $LOG_DIR/sketch.log
 docker-compose -f $SCRIPT_DIR/local-docker-compose.yml logs database > $LOG_DIR/database.log
