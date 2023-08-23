116c3aecc2098ba2ba3b7d03a546667b03ead7fb
Rafael Fiume <rafaelfiume@users.noreply.github.com>
2023-08-23 09:33:11 +0100

    Update http4s-circe, http4s-dsl, ... to 1.0.0-M40 (#132)
     * I had to pach this one myself

--- service/src/main/scala/org/fiume/sketch/app/Server.scala
+++ service/src/main/scala/org/fiume/sketch/app/Server.scala
@@ -9,15 +9,17 @@ import org.fiume.sketch.auth0.http.AuthRoutes
 import org.fiume.sketch.auth0.http.middlewares.Auth0Middleware
 import org.fiume.sketch.http.HealthStatusRoutes
 import org.fiume.sketch.storage.documents.http.DocumentsRoutes
-import org.http4s.HttpRoutes
+import org.http4s.{HttpApp, HttpRoutes}
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits.*
 import org.http4s.server.Server
-import org.typelevel.log4cats.slf4j.Slf4jLogger
+import org.typelevel.log4cats.LoggerFactory
+import org.typelevel.log4cats.slf4j.{Slf4jFactory, Slf4jLogger}
 
 object Server:
 
   def run[F[_]: Network]()(using F: Async[F]): F[ExitCode] =
+    given LoggerFactory[F] = Slf4jFactory.create[F]
     val logger = Slf4jLogger.getLogger[F]
     (for
       conf <- Resource.eval(ServiceConfig.load[F])
@@ -32,29 +34,35 @@ object Server:
         logger.error(s"The service has failed with $ex")
       }
 
-  private def httpServer[F[_]: Async: Network](resources: Resources[F]): Resource[F, Server] =
-    val httpApp = HttpApi.httpApp[F](resources).orNotFound
-    EmberServerBuilder.default
-      .withHost(host"0.0.0.0")
-      .withPort(port"8080")
-      .withHttpApp(httpApp)
-      .build
+  private def httpServer[F[_]: Async: Network: LoggerFactory](resources: Resources[F]): Resource[F, Server] =
+    val server =
+      HttpApi.httpApp[F](resources).map { httpApp =>
+        EmberServerBuilder
+          .default[F]
+          .withHost(host"0.0.0.0")
+          .withPort(port"8080")
+          .withHttpApp(httpApp)
+      }
+    Resource.suspend(server.map(_.build))
 
 object HttpApi:
   import org.http4s.server.middleware.*
   import org.http4s.headers.Origin
   import org.http4s.Uri
 
-  def httpApp[F[_]: Async](res: Resources[F]): HttpRoutes[F] =
-    val corsMiddleware = CORS.policy.withAllowOriginHost(Set(Origin.Host(Uri.Scheme.http, Uri.RegName("localhost"), 5173.some)))
+  def httpApp[F[_]: Async: LoggerFactory](res: Resources[F]): F[HttpApp[F]] =
     val authMiddleware = Auth0Middleware(res.authenticator)
 
-    val authRoutes = new AuthRoutes[F](enableLogging = true)(res.authenticator).router()
-    val documentsRoutes =
+    val authRoutes: HttpRoutes[F] = new AuthRoutes[F](enableLogging = true)(res.authenticator).router()
+    val documentsRoutes: HttpRoutes[F] =
       new DocumentsRoutes[F, ConnectionIO](enableLogging = false, res.customWorkerThreadPool, authMiddleware)(res.documentsStore)
         .router()
-    val healthStatusRoutes = new HealthStatusRoutes[F](res.customWorkerThreadPool, res.versions, res.healthCheck).router()
+    val healthStatusRoutes: HttpRoutes[F] =
+      new HealthStatusRoutes[F](res.customWorkerThreadPool, res.versions, res.healthCheck).router()
 
-    healthStatusRoutes <+>
-      corsMiddleware(authRoutes) <+>
-      corsMiddleware(documentsRoutes)
+    val corsMiddleware: CORSPolicy =
+      CORS.policy.withAllowOriginHost(Set(Origin.Host(Uri.Scheme.http, Uri.RegName("localhost"), 5173.some)))
+    for
+      cAuthRoutes <- corsMiddleware.httpRoutes[F](authRoutes)
+      cDocsRoutes <- corsMiddleware.httpRoutes[F](documentsRoutes)
+    yield (healthStatusRoutes <+> cAuthRoutes <+> cDocsRoutes).orNotFound
--- test-acceptance/src/test/scala/org/fiume/sketch/acceptance/testkit/Http4sClientContext.scala
+++ test-acceptance/src/test/scala/org/fiume/sketch/acceptance/testkit/Http4sClientContext.scala
@@ -7,10 +7,13 @@ import org.http4s.client.*
 import org.http4s.client.dsl.io.*
 import org.http4s.client.middleware.{Retry, RetryPolicy}
 import org.http4s.ember.client.*
+import org.typelevel.log4cats.LoggerFactory
+import org.typelevel.log4cats.slf4j.Slf4jFactory
 
 import scala.concurrent.duration.*
 
 trait Http4sClientContext:
+  given LoggerFactory[IO] = Slf4jFactory.create[IO]
   def withHttp[A](exec: Client[IO] => IO[A]): IO[A] = EmberClientBuilder
     .default[IO]
     .build
