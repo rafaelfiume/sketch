9a9783cb6e1e66bb913aa17ed540068375ad006d
Rafael Fiume <rafaelfiume@users.noreply.github.com>
2023-10-21 10:46:21 +0100

    Update sbt-scalafmt to 2.5.2 (#148)

--- project/project/Plugin.scala
+++ project/project/Plugin.scala
@@ -9,7 +9,7 @@ object Plugin {
     val OrganizeImports     = "0.6.0"
     val SbtNativePackager   = "1.9.16"
     val Scalafix            = "0.11.0"
-    val ScalaFmt            = "2.5.0"
+    val ScalaFmt            = "2.5.2"
     val Tpolecat            = "0.4.4"
   }
 
