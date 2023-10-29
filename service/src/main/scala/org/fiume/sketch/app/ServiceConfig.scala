ffa52f00c3c82bfd1495e42f705baaeae07cec9a
Rafael Fiume <rafael.fiume@yahoo.it>
2023-10-29 17:06:14 +0000

    Extract `DatabaseConfig.load`

--- service/src/main/scala/org/fiume/sketch/app/ServiceConfig.scala
+++ service/src/main/scala/org/fiume/sketch/app/ServiceConfig.scala
@@ -6,7 +6,6 @@ import ciris.*
 import org.fiume.sketch.auth0.KeyStringifier
 import org.fiume.sketch.shared.app.algebras.Versions.Environment
 import org.fiume.sketch.storage.DatabaseConfig
-import org.http4s.Uri
 
 import java.security.interfaces.{ECPrivateKey, ECPublicKey}
 
@@ -19,7 +18,6 @@ case class ServiceConfig(
 case class EcKeyPairConfig(privateKey: ECPrivateKey, publicKey: ECPublicKey)
 
 object ServiceConfig:
-  given ConfigDecoder[String, Uri] = ConfigDecoder[String].map(Uri.unsafeFromString)
   given ConfigDecoder[String, Environment] = ConfigDecoder[String].map(Environment.apply)
   given ConfigDecoder[String, ECPrivateKey] =
     ConfigDecoder[String].mapEither((_, key) => KeyStringifier.ecPrivateKeyFromPem(key).leftMap(ConfigError(_)))
@@ -29,19 +27,11 @@ object ServiceConfig:
   def load[F[_]: Async]: F[ServiceConfig] =
     (for
       environment <- env("ENV").as[Environment]
-      jdbcUrl <- env("DB_URL").as[Uri]
-      dbUser <- env("DB_USER")
-      dbPassword <- env("DB_PASS").secret
       privateKey <- env("PRIVATE_KEY").as[ECPrivateKey].redacted
       publicKey <- env("PUBLIC_KEY").as[ECPublicKey].redacted
+      databaseConfig <- DatabaseConfig.load[F](dbPoolThreads = 10)
     yield ServiceConfig(
       env = environment,
       keyPair = EcKeyPairConfig(privateKey, publicKey),
-      db = DatabaseConfig(
-        driver = "org.postgresql.Driver",
-        uri = jdbcUrl,
-        user = dbUser,
-        password = dbPassword,
-        dbPoolThreads = 10
-      )
+      db = databaseConfig
     )).load[F]
--- storage/src/main/scala/org/fiume/sketch/storage/DatabaseConfig.scala
+++ storage/src/main/scala/org/fiume/sketch/storage/DatabaseConfig.scala
@@ -8,7 +8,22 @@ case class DatabaseConfig(
   uri: Uri,
   user: String,
   password: Secret[String],
-  // Match the default size of the Hikari pool
-  // Also https://github.com/brettwooldridge/HikariCP/wiki/About-Pool-Sizing
-  dbPoolThreads: Int = 10
+  dbPoolThreads: Int
 )
+
+object DatabaseConfig:
+  given ConfigDecoder[String, Uri] = ConfigDecoder[String].map(Uri.unsafeFromString)
+
+  // For `dbPoolThreads`, see https://github.com/brettwooldridge/HikariCP/wiki/About-Pool-Sizing#the-formula
+  def load[F[_]](dbPoolThreads: Int): ConfigValue[F, DatabaseConfig] =
+    for
+      jdbcUrl <- env("DB_URL").as[Uri]
+      dbUser <- env("DB_USER")
+      dbPassword <- env("DB_PASS").secret
+    yield DatabaseConfig(
+        driver = "org.postgresql.Driver",
+        uri = jdbcUrl,
+        user = dbUser,
+        password = dbPassword,
+        dbPoolThreads = dbPoolThreads
+    )
