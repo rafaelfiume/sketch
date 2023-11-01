dc296dfc8fa6232bced59580b96cd266263b1c23
Rafael Fiume <rafael.fiume@yahoo.it>
2023-11-01 13:54:41 +0000

    Experiment with logs

--- shared-components/src/main/scala/org/fiume/sketch/shared/app/http4s/middlewares/TraceAuditLogMiddleware.scala
+++ shared-components/src/main/scala/org/fiume/sketch/shared/app/http4s/middlewares/TraceAuditLogMiddleware.scala
@@ -11,16 +11,20 @@ object TraceAuditLogMiddleware:
 
   def apply[F[_]: Async](logger: Logger[F], enableLogging: Boolean)(routes: HttpRoutes[F]): HttpRoutes[F] =
     // logs can have a tremend impact on the service performance
-    extension (message: String) def info = if enableLogging then OptionT.liftF(logger.info(message)) else OptionT.pure[F](())
+    extension (message: String)
+      def info = if enableLogging then OptionT.liftF(logger.info(message)) else OptionT.pure[F](())
+      def warn = if enableLogging then OptionT.liftF(logger.warn(message)) else OptionT.pure[F](())
 
     Kleisli { req =>
       val correlationId = req.headers.get(CIString("X-Correlation-ID")).getOrElse(TraceId.make().toString)
       /// val user = req.headers.get(CIString("X-Authenticated-Username")) UUID?? or anonymous
-      s"Received request: ${req.method} ${req.uri} ${req.headers} (correlationId=$correlationId)".info *>
-        routes.run(req).flatMap { res =>
-          s"Sent response: ${res.status} (correlationId=$correlationId)".info *>
-            OptionT.pure(res)
-        }
+      (for
+        _ <- s"Received request: ${req.method} ${req.uri} ${req.headers} (correlationId=$correlationId)".info
+        res <- routes.run(req)
+        _ <- s"Sent response: ${res.status} (correlationId=$correlationId)".info
+      yield res).onError { error =>
+        s"Request (correlationId=$correlationId) failed with: ${error.getMessage()}".warn
+      }
     }
 
 object TraceId:
