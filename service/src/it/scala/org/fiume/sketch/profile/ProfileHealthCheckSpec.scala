b4d02ee980f30fec6bcffacb0c7dd2e1d175c4c5
Rafael Fiume <rafael.fiume@yahoo.it>
2023-11-18 17:59:36 +0000

    Extract `HttpServiceContext` to `testkit` package

--- service/src/it/scala/org/fiume/sketch/profile/ProfileHealthCheckSpec.scala
+++ service/src/it/scala/org/fiume/sketch/profile/ProfileHealthCheckSpec.scala
@@ -1,13 +1,19 @@
 package org.fiume.sketch.profile
 
-import cats.effect.IO
-import com.comcast.ip4s.port
+import cats.effect.{IO, Resource}
+import cats.implicits.*
+import com.comcast.ip4s.{port, *}
 import munit.Assertions.*
 import munit.CatsEffectSuite
 import org.fiume.sketch.profile.ProfileHealthCheck.ProfileServiceConfig
 import org.fiume.sketch.shared.app.ServiceStatus.{DependencyStatus, Status}
 import org.fiume.sketch.shared.app.ServiceStatus.Dependency.*
 import org.fiume.sketch.shared.testkit.FileContentContext
+import org.fiume.sketch.testkit.HttpServiceContext
+import org.http4s.*
+import org.http4s.circe.CirceEntityEncoder.*
+import org.http4s.dsl.io.*
+import org.http4s.server.Router
 import org.scalacheck.{Gen, ShrinkLowPriority}
 
 class ProfileHealthCheckSpec extends CatsEffectSuite with ProfileHealthCheckSpecContext with ShrinkLowPriority:
@@ -33,7 +39,7 @@ class ProfileHealthCheckSpec extends CatsEffectSuite with ProfileHealthCheckSpec
       }
 
   test("check if rustic (Profile) is behaving"):
-    profileStatusIsInWeirdState()
+    profileStatusInWeirdState()
       .flatMap { port => ProfileHealthCheck.make[IO](config = ProfileServiceConfig(localhost, port)) }
       .use { healthCheck =>
         for
@@ -52,18 +58,7 @@ class ProfileHealthCheckSpec extends CatsEffectSuite with ProfileHealthCheckSpec
         yield ()
       }
 
-trait ProfileHealthCheckSpecContext extends FileContentContext:
-  import cats.effect.*, org.http4s.*, org.http4s.dsl.io.*
-  import cats.implicits.*
-  import com.comcast.ip4s.*
-  import org.http4s.ember.server.*
-  import org.http4s.implicits.*
-  import org.http4s.server.Router
-  import org.typelevel.log4cats.LoggerFactory
-  import org.typelevel.log4cats.slf4j.Slf4jFactory
-  import org.http4s.server.Server
-  import org.http4s.circe.CirceEntityEncoder.*
-
+trait ProfileHealthCheckSpecContext extends FileContentContext with HttpServiceContext:
   val localhost = Host.fromString("localhost").getOrElse(throw new AssertionError("localhost is valid host"))
 
   val healthy = "contract/shared/app/http/servicestatus.healthy.json"
@@ -72,44 +67,27 @@ trait ProfileHealthCheckSpecContext extends FileContentContext:
   def profileStatusIs(pathToResponsePayload: String): Resource[IO, Port] =
     for
       port <- Resource.eval(freePort())
-      httpApp <- statusRoute(pathToResponsePayload)
+      httpApp <- runningStatusRoute(pathToResponsePayload)
       _ <- makeServer(port)(httpApp).void
     yield port
 
-  def profileStatusIsInWeirdState(): Resource[IO, Port] =
+  def profileStatusInWeirdState(): Resource[IO, Port] =
     for
       port <- Resource.eval(freePort())
       httpApp <- weirdStatusRoute()
       _ <- makeServer(port)(httpApp).void
     yield port
 
-  private def statusRoute(pathToResponsePayload: String): Resource[IO, HttpRoutes[IO]] =
-    jsonFrom[IO](pathToResponsePayload, debug = false).map { serviceStatus =>
-      val route = HttpRoutes.of[IO] { case GET -> Root / "status" => Ok(serviceStatus) }
-      Router("/" -> route)
-    }
+  private def runningStatusRoute(pathToResponsePayload: String): Resource[IO, HttpRoutes[IO]] =
+    jsonFrom[IO](pathToResponsePayload, debug = false)
+      .map { serviceStatus =>
+        makeStatusRoute(willRespond = Ok(serviceStatus))
+      }
 
   private def weirdStatusRoute(): Resource[IO, HttpRoutes[IO]] =
     def response: Gen[IO[Response[IO]]] = Gen.oneOf(InternalServerError(), BadRequest())
-    Resource.pure {
-      val route = HttpRoutes.of[IO] { case GET -> Root / "status" => response.sample.get }
-      Router("/" -> route)
-    }
+    Resource.pure { makeStatusRoute(willRespond = response.sample.get) }
 
-  given LoggerFactory[IO] = Slf4jFactory.create[IO]
-  private def makeServer(port: Port)(httpApp: HttpRoutes[IO]): Resource[IO, Server] =
-    EmberServerBuilder
-      .default[IO]
-      .withHost(ipv4"0.0.0.0")
-      .withPort(port)
-      .withHttpApp(httpApp.orNotFound)
-      .build
-
-  private def freePort(): IO[Port] =
-    Resource
-      .fromAutoCloseable(IO.delay { new java.net.ServerSocket(0) })
-      .use { socket =>
-        IO.delay {
-          Port.fromInt { socket.getLocalPort() }.getOrElse(throw new AssertionError("there must be a free port"))
-        }
-      }
+  private def makeStatusRoute(willRespond: IO[Response[IO]]) =
+    val route = HttpRoutes.of[IO] { case GET -> Root / "status" => willRespond }
+    Router("/" -> route)
