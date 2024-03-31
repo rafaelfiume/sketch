d2f7580fd5703c67ae7b0adf2b9bee72c52a4edb
Scala Steward <rafael.fiume@yahoo.it>
2024-03-31 23:42:52 +0100

    Update cats-effect to 3.5.4

--- project/Dependency.scala
+++ project/Dependency.scala
@@ -8,7 +8,7 @@ object Dependency {
     val bouncycastle          = "1.76"
 
     val cats                  = "2.10.0"
-    val catsEffect            = "3.5.2"
+    val catsEffect            = "3.5.4"
     val circe                 = "0.14.6"
     val ciris                 = "3.3.0"
     val doobie                = "1.0.0-RC4"
