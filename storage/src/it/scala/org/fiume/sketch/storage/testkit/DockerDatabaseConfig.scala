26ebc31490313ad87324126afc0c4967e670bc11
Rafael Fiume <rafael.fiume@yahoo.it>
2024-04-06 14:23:12 +0100

    Updates PostgreSQL version

--- storage/src/it/scala/org/fiume/sketch/storage/testkit/DockerDatabaseConfig.scala
+++ storage/src/it/scala/org/fiume/sketch/storage/testkit/DockerDatabaseConfig.scala
@@ -1,7 +1,7 @@
 package org.fiume.sketch.storage.testkit
 
 object DockerDatabaseConfig:
-  val postgreSQLVersion = "15.3-bullseye"
+  val postgreSQLVersion = "16.2-bullseye"
   val driver = "org.postgresql.Driver"
   val database = "sketch"
   val user = "sketch.dev"
