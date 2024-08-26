6e0db887c02b0fa60b4d4adb8c6704b0c83b76a3
Scala Steward <rafael.fiume@yahoo.it>
2024-08-26 22:33:14 +0100

    Update log4cats-slf4j to 2.7.0

--- project/Dependency.scala
+++ project/Dependency.scala
@@ -15,7 +15,7 @@ object Dependency {
     val flyway                = "10.10.0"
     val fs2                   = "3.11.0"
     val http4s                = "1.0.0-M41"
-    val log4cats              = "2.6.0"
+    val log4cats              = "2.7.0"
     val slf4j                 = "2.0.12"
 
     val gatling               = "3.9.5"
