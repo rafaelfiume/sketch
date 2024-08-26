a6fa3c0dd234a38eecfb6d527a7e532dfc42cb19
Rafael Fiume <rafaelfiume@users.noreply.github.com>
2024-08-27 00:12:49 +0100

    Update sbt-scalafix to 0.12.1 (#188)

--- project/project/Plugin.scala
+++ project/project/Plugin.scala
@@ -6,7 +6,7 @@ object Plugin {
     val Gatling             = "4.9.2"
     val Git                 = "2.0.1"
     val SbtNativePackager   = "1.9.16"
-    val Scalafix            = "0.12.0"
+    val Scalafix            = "0.12.1"
     val ScalaFmt            = "2.5.2"
     val Tpolecat            = "0.5.2"
   }
