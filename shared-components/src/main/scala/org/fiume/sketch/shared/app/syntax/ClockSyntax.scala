798c35d645264548b84e258bc9a778d5cf374fbd
Rafael Fiume <rafael.fiume@yahoo.it>
2024-09-14 23:08:19 +0100

    Rename to ClockSyntax

--- shared-components/src/main/scala/org/fiume/sketch/shared/app/syntax/ClockSyntax.scala
+++ shared-components/src/main/scala/org/fiume/sketch/shared/app/syntax/ClockSyntax.scala
@@ -7,7 +7,7 @@ import cats.implicits.*
 import java.time.ZoneOffset.UTC
 import java.time.ZonedDateTime
 
-object ClockOps: // there might be a better package to Ops than `algebras`?
+object ClockSyntax:
   extension [F[_]: Functor](clock: Clock[F])
     def getNow(): F[ZonedDateTime] =
       clock.realTimeInstant.map { instant => ZonedDateTime.ofInstant(instant, UTC) }
