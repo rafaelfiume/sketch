1862c410ef819df8a9922721a530dd943d54351b
Rafael Fiume <rafael.fiume@yahoo.it>
2024-11-03 10:43:59 +0000

    Improves acc test

--- test-acceptance/src/test/scala/org/fiume/sketch/acceptance/auth0/UserAccountsManagementAccSpec.scala
+++ test-acceptance/src/test/scala/org/fiume/sketch/acceptance/auth0/UserAccountsManagementAccSpec.scala
@@ -9,9 +9,12 @@ import org.fiume.sketch.shared.account.management.http.HttpUsersClient
 import org.fiume.sketch.shared.auth.http.HttpAuthClient
 import org.fiume.sketch.shared.auth.testkit.PasswordsGens.*
 import org.fiume.sketch.shared.auth.testkit.UserGens.*
+import org.fiume.sketch.shared.authorisation.AuthorisationError
 import org.fiume.sketch.shared.testkit.Http4sClientContext
 import org.fiume.sketch.shared.testkit.syntax.EitherSyntax.*
 import org.fiume.sketch.shared.testkit.syntax.OptionSyntax.*
+
+import java.time.Instant
 
 class UserAccountsManagementAccSpec extends CatsEffectSuite with Http4sClientContext:
 
@@ -24,9 +27,17 @@ class UserAccountsManagementAccSpec extends CatsEffectSuite with Http4sClientCon
         authClient = HttpAuthClient.make(HttpAuthClient.Config(host"localhost", port"8080"), http)
         jwt <- authClient.login(username, password).map(_.rightOrFail)
 
-        accountClient = HttpUsersClient.make(HttpUsersClient.Config(host"localhost", port"8080"), http)
-        result <- accountClient.markAccountForDeletion(userId, jwt).map(_.rightOrFail)
+        usersClient = HttpUsersClient.make(HttpUsersClient.Config(host"localhost", port"8080"), http)
+        result <- usersClient.markAccountForDeletion(userId, jwt).map(_.rightOrFail)
 
+        _ <- IO {
+          assertEquals(result.userId, userId)
+          assert(result.permanentDeletionAt.isAfter(Instant.now()))
+        }
+        _ <- assertIO(
+          usersClient.restoreAccount(userId, jwt).map(_.leftOrFail),
+          AuthorisationError.UnauthorisedError
+        )
         unauthorised <- authClient.login(username, password)
       yield unauthorised.leftOrFail
     }
