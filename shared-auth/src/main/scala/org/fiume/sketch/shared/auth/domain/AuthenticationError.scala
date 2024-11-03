ac949bfbd694142855b3775115047c5619e37519
Rafael Fiume <rafael.fiume@yahoo.it>
2024-11-03 23:02:47 +0000

    Remove comments [skip ci]

--- shared-auth/src/main/scala/org/fiume/sketch/shared/auth/domain/AuthenticationError.scala
+++ shared-auth/src/main/scala/org/fiume/sketch/shared/auth/domain/AuthenticationError.scala
@@ -1,10 +1,5 @@
 package org.fiume.sketch.shared.auth.domain
 
-/*
- * These errors occur specifically during the login path of authentication.
- * For the jwt verification phase of authentication, see JwtError
- * TODO Should AuthenticationError be renamed?
- */
 enum AuthenticationError(val details: String):
   case UserNotFoundError extends AuthenticationError("User not found")
   case InvalidPasswordError extends AuthenticationError("Invalid password")
