eb9d2f4428710a7df02e1ef64386468c65546aff
Rafael Fiume <rafael.fiume@yahoo.it>
2025-01-14 19:30:38 +0000

    Implement and test UserDataDeletionJob

--- service/src/main/scala/org/fiume/sketch/users/UserDataDeletionJob.scala
+++ service/src/main/scala/org/fiume/sketch/users/UserDataDeletionJob.scala
@@ -1,13 +1,28 @@
 package org.fiume.sketch.users
 
-import cats.Functor
+import cats.Monad
+import cats.effect.kernel.Sync
 import cats.implicits.*
-import org.fiume.sketch.shared.auth.accounts.AccountDeletedNotificationConsumer
+import org.fiume.sketch.shared.auth.UserId
+import org.fiume.sketch.shared.auth.accounts.{AccountDeletedNotification, AccountDeletedNotificationConsumer}
+import org.fiume.sketch.shared.common.EntityId
+import org.fiume.sketch.shared.common.app.syntax.StoreSyntax.commitStream
+import org.fiume.sketch.shared.common.events.EventId
 import org.fiume.sketch.shared.common.jobs.Job
 import org.fiume.sketch.shared.domain.documents.algebras.DocumentsStore
+import org.fiume.sketch.users.UserDataDeletionJob.JobReport
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+import org.typelevel.log4cats.syntax.LoggerInterpolator
 
 object UserDataDeletionJob:
-  def make[F[_], Txn[_]: Functor](
+  case class JobReport(
+    triggeringEventId: EventId,
+    deletedUserId: UserId, // The user whose account has been deleted
+    deletedEntities: List[EntityId[?]] // The id of the entities deleted as result of the user account deletions
+  )
+
+  def make[F[_]: Sync, Txn[_]: Monad](
     accountDeletedNotificationConsumer: AccountDeletedNotificationConsumer[Txn],
     store: DocumentsStore[F, Txn]
   ) =
@@ -16,16 +31,48 @@ object UserDataDeletionJob:
       store
     )
 
-private class UserDataDeletionJob[F[_], Txn[_]: Functor] private (
+private class UserDataDeletionJob[F[_]: Sync, Txn[_]: Monad] private (
   accountDeletedNotificationConsumer: AccountDeletedNotificationConsumer[Txn],
   store: DocumentsStore[F, Txn]
-) extends Job[F, Unit]: // unit for now
+) extends Job[F, Option[JobReport]]:
+
+  // enable Store's syntax
+  given DocumentsStore[F, Txn] = store
+
+  /*
+   * There appears to be a way to instantiate a Logger without requiring `Sync`,
+   * but it seems a bit convoluted to me.
+   * See https://typelevel.org/log4cats/#logging-using-capabilities
+   */
+  given Logger[F] = Slf4jLogger.getLogger[F]
+
   override val description: String = "Deletes user data following account deletion"
-  override def run(): F[Unit] =
-    // consume event
-    // fetch all documents of a user
-    // delete all documents of a user
-    val job = accountDeletedNotificationConsumer
-      .consumeEvent()
-      .void // for now
-    store.commit { job }
+
+  override def run(): F[Option[JobReport]] =
+    var notif0: Option[AccountDeletedNotification.Notified] = None
+    val job = fs2.Stream
+      .evals(accountDeletedNotificationConsumer.consumeEvent())
+      .flatMap { notif =>
+        notif0 = notif.some
+        process(notif)
+      }
+    job.commitStream().compile.toList.map { affectedEntityIds =>
+      notif0.map(n => JobReport(n.uuid, n.userId, affectedEntityIds))
+    }
+
+  private def process(event: AccountDeletedNotification.Notified): fs2.Stream[Txn, EntityId[?]] =
+    store
+      .fetchDocumentsByOwnerId(event.userId)
+      .evalMap { doc => store.delete(doc.uuid) }
+      .unNone
+  /*
+   * Note: Using `parEvalMapUnbounded`instead of `evalMap` would have been nice.
+   * However, that would require an instance of `cats.effect.Concurrent[ConnectionIO]`, which isn't available.
+   * See https://github.com/typelevel/doobie/issues/1561
+   */
+
+  private def info(eventId: EventId, userId: UserId, entityIds: List[EntityId[?]]): Txn[Unit] =
+    val l = s"UserDataDeletionJob completed successfully: triggeringEventId=${eventId}, " +
+      s"deletedUserId=${userId}, " +
+      s"affectedEntityIds=${entityIds.map(id => s"[UUID: ${id}, Entity Type: ${id.entityType}]").mkString(", ")}}"
+    store.lift { info"$l" }
---
+++ service/src/test/scala/org/fiume/sketch/users/UserDataDeletionJobSpec.scala
@@ -0,0 +1,52 @@
+package org.fiume.sketch.users
+
+import cats.effect.IO
+import munit.{CatsEffectSuite, ScalaCheckEffectSuite}
+import org.fiume.sketch.shared.auth.accounts.AccountDeletedNotification
+import org.fiume.sketch.shared.auth.accounts.AccountDeletedNotification.ToNotify
+import org.fiume.sketch.shared.auth.testkit.EventsFlowContext.EventConsumerContext
+import org.fiume.sketch.shared.common.events.{EventId, Recipient}
+import org.fiume.sketch.shared.common.testkit.EventGens.given
+import org.fiume.sketch.shared.domain.documents.DocumentWithIdAndStream
+import org.fiume.sketch.shared.domain.testkit.DocumentsGens.given
+import org.fiume.sketch.shared.domain.testkit.DocumentsStoreContext
+import org.fiume.sketch.shared.testkit.syntax.OptionSyntax.someOrFail
+import org.fiume.sketch.users.UserDataDeletionJob.JobReport
+import org.scalacheck.ShrinkLowPriority
+import org.scalacheck.effect.PropF.forAllF
+
+class UserDataDeletionJobSpec
+    extends CatsEffectSuite
+    with ScalaCheckEffectSuite
+    with EventConsumerContext[ToNotify]
+    with DocumentsStoreContext
+    with ShrinkLowPriority:
+
+  override def scalaCheckTestParameters = super.scalaCheckTestParameters.withMinSuccessfulTests(10)
+
+  test("deletes user data when a user's account notification is received"):
+    forAllF { (triggeringEventId: EventId, fstDoc: DocumentWithIdAndStream[IO], sndDoc: DocumentWithIdAndStream[IO]) =>
+      val userId = sndDoc.metadata.ownerId
+      for
+        eventConsumer <- makeEventConsumer(nextEvent =
+          AccountDeletedNotification.notified(triggeringEventId, userId, Recipient("job-pbt"))
+        )
+        store <- makeDocumentsStore(state = fstDoc, sndDoc)
+        job = UserDataDeletionJob.make(eventConsumer, store)
+
+        result <- job.run().map(_.someOrFail)
+//
+      yield assertEquals(result, JobReport(triggeringEventId, userId, List(sndDoc.uuid)))
+    }
+
+  test("skips processing if there are no account's deletion notfications"):
+    forAllF { (fstDoc: DocumentWithIdAndStream[IO], sndDoc: DocumentWithIdAndStream[IO]) =>
+      for
+        eventConsumer <- makeEmptyEventConsumer()
+        store <- makeDocumentsStore(state = fstDoc, sndDoc)
+        job = UserDataDeletionJob.make(eventConsumer, store)
+
+        result <- job.run()
+//
+      yield assertEquals(result, None)
+    }
