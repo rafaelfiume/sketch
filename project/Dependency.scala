c12bfc44df0d8a3a791abcd0f28973f4aabdc914
Scala Steward <rafael.fiume@yahoo.it>
2025-06-13 21:33:56 +0100

    Reformat with scalafmt 3.8.6
    Executed command: scalafmt --non-interactive

--- project/Dependency.scala
+++ project/Dependency.scala
@@ -1,4 +1,4 @@
-import sbt._
+import sbt.*
 
 // format: off
 object Dependency {
@@ -23,7 +23,7 @@ object Dependency {
     val munit                 = "1.0.1"
     val munitCatsEffect       = "2.0.0"
     val munitDiscipline       = "2.0.0"
-    val munitScalaCheck       = "1.0.0" 
+    val munitScalaCheck       = "1.0.0"
     val munitScalaCheckEffect = "1.0.4"
     val munitTestcontainers   = "0.41.4"
   }
