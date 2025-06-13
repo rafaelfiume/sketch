64e1da1d0c560db99bd7344f0fead85094df9f66
Rafael Fiume <rafaelfiume@users.noreply.github.com>
2025-06-13 22:24:47 +0100

    Update cats-core, cats-laws to 2.13.0 (#249)

--- project/Dependency.scala
+++ project/Dependency.scala
@@ -7,7 +7,7 @@ object Dependency {
     val jwtCirce              = "10.0.1"
     val bouncycastle          = "1.78.1"
 
-    val cats                  = "2.12.0"
+    val cats                  = "2.13.0"
     val catsEffect            = "3.5.4"
     val circe                 = "0.14.9"
     val ciris                 = "3.6.0"
