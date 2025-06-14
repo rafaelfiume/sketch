710b5f853156abeae8a8669474f7739c2bd8ca4b
Rafael Fiume <rafael.fiume@yahoo.it>
2025-06-14 21:35:11 +0100

    Fix build error after dependency update

--- storage/src/main/scala/org/fiume/sketch/storage/postgres/AbstractPostgresStore.scala
+++ storage/src/main/scala/org/fiume/sketch/storage/postgres/AbstractPostgresStore.scala
@@ -1,13 +1,13 @@
 package org.fiume.sketch.storage.postgres
 
-import cats.effect.Sync
+import cats.effect.Concurrent
 import cats.~>
 import doobie.ConnectionIO
 import doobie.implicits.*
 import doobie.util.transactor.Transactor
 import org.fiume.sketch.shared.common.app.Store
 
-abstract class AbstractPostgresStore[F[_]: Sync] protected (l: F ~> ConnectionIO, tx: Transactor[F])
+abstract class AbstractPostgresStore[F[_]: Concurrent] protected (l: F ~> ConnectionIO, tx: Transactor[F])
     extends Store[F, ConnectionIO]:
 
   override val lift: [A] => F[A] => ConnectionIO[A] = [A] => (fa: F[A]) => l(fa)
