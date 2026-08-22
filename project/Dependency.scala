f239ac397d87cfc3b10f2ac99856733f4e1270e2
Scala Steward <rafael.fiume@yahoo.it>
2026-08-22 15:02:38 -0300

    Update munit-cats-effect to 2.2.0

--- project/Dependency.scala
+++ project/Dependency.scala
@@ -22,7 +22,7 @@ object Dependency {
     val gatling               = "3.11.5"
     val logstash              = "8.0"
     val munit                 = "1.1.1"
-    val munitCatsEffect       = "2.1.0"
+    val munitCatsEffect       = "2.2.0"
     val munitDiscipline       = "2.0.0"
     val munitScalaCheck       = "1.1.0"
     val munitScalaCheckEffect = "1.0.4"
